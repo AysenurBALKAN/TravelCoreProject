38b728869434cfc5693e6fbf41453885cb3839b5
AysenurBALKAN <balkan.aysenur@gmail.com>
2023-12-26 12:10:14 +0300

    partial view and destination list

---
+++ ViewComponents/_PopularDestinations.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TBusinessLayer.Concrete;
+using TDataAccesLayer.EntityFramework;
+
+namespace TravelCoreProject.ViewComponents
+{
+    public class _PopularDestinations:ViewComponent
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.TGetlist();
+            return View(values);
+            
+        }
+    }
+}
