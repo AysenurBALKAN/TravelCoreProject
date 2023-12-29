92fc34dfe632f141a1a9c755968f141fae7b3118
AysenurBALKAN <balkan.aysenur@gmail.com>
2023-12-29 13:08:07 +0300

    Destination Details

---
+++ Controllers/DestinationController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TBusinessLayer.Concrete;
+using TDataAccesLayer.EntityFramework;
+
+namespace TravelCoreProject.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetlist();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            var values = destinationManager.TGetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult DestinationDetails(DestinationController p)
+        {
+            return View();
+        }
+    }
+}
