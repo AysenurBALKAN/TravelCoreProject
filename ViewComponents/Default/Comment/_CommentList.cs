6b2d6638f6a95bde74ead7b2cfa912af928ca71f
AysenurBALKAN <balkan.aysenur@gmail.com>
2024-01-20 13:18:26 +0300

    destination and comment

---
+++ ViewComponents/Default/Comment/_CommentList.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TBusinessLayer.Concrete;
+using TDataAccesLayer.EntityFramework;
+
+namespace TravelCoreProject.ViewComponents.Default.Comment
+{
+    public class _CommentList : ViewComponent
+    {
+        CommentManager commentManager = new CommentManager(new EfComment());
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = commentManager.TGetDestinationById(id);
+            return View(values);
+        }
+    }
+}
