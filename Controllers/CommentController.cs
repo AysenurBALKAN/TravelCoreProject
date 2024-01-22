1840e2fddac6494fb563b79fa0b3fbdb4c3c9e84
AysenurBALKAN <balkan.aysenur@gmail.com>
2024-01-22 16:42:31 +0300

    add_Comment

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TBusinessLayer.Concrete;
+using TDataAccesLayer.EntityFramework;
+using TEntityLayer.Concrete;
+
+namespace TravelCoreProject.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EfComment());
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult AddComment(Comment p)
+        {
+            p.CommentDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.CommentState = true;
+            p.DestinationId = 3;
+            commentManager.TAdd(p);
+            return RedirectToAction("Index","Destination");
+        }
+    }
+}
