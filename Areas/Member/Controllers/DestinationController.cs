6f3336bed6c9ed6dab4f618aab9adf570f112955
AysenurBALKAN <balkan.aysenur@gmail.com>
2024-03-02 13:14:11 +0300

    routes listed in members profile

---
+++ Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TBusinessLayer.Concrete;
+using TDataAccesLayer.EntityFramework;
+
+namespace TravelCoreProject.Areas.Member.Controllers
+{
+    [AllowAnonymous]
+    [Area("Member")]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetlist();
+            return View(values);
+        }
+    }
+}
