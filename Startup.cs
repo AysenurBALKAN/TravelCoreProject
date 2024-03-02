de06ab0c8f881099902fc78870bf49cdbbfa8aa3
AysenurBALKAN <balkan.aysenur@gmail.com>
2024-03-02 11:12:15 +0300

    Member Area

--- Startup.cs
+++ Startup.cs
@@ -70,6 +70,13 @@ namespace TravelCoreProject
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
