63ca517d10d06e155b539e34d184ae779fad97a8
AysenurBALKAN <balkan.aysenur@gmail.com>
2024-03-03 08:15:33 +0300

    Profile Page and Login

--- Areas/Member/Controllers/ProfileController.cs
+++ Areas/Member/Controllers/ProfileController.cs
@@ -1,16 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TEntityLayer.Concrete;
+using TravelCoreProject.Areas.Member.Models;
 
 namespace TravelCoreProject.Areas.Member.Controllers
 {
+    [Area("Member")]
+    [Route("Member/[controller]/[action]")]
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+        public ProfileController(UserManager<AppUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task< IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel userEditViewModel=new UserEditViewModel();
+            userEditViewModel.name = values.Name;
+            userEditViewModel.surname = values.Surname;
+            userEditViewModel.phonenumber = values.PhoneNumber;
+            userEditViewModel.mail = values.Email;
+
+            return View(userEditViewModel);
+
         }
     }
 }
---
+++ Models/UserSignInViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelCoreProject.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage ="Lütfen Kullanıcı Adınızı Girniz.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifrenizi Girniz.")]
+        public string Password { get; set; }
+    }
+}
