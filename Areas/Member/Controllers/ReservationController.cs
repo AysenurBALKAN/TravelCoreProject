3acb4892ff08be9b1ae94547d5fdfcb30c5a2178
AysenurBALKAN <balkan.aysenur@gmail.com>
2024-03-05 08:59:08 +0300

    Reservation Made

--- Areas/Member/Controllers/ReservationController.cs
+++ Areas/Member/Controllers/ReservationController.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TBusinessLayer.Concrete;
+using TDataAccesLayer.EntityFramework;
 using TEntityLayer.Concrete;
 
 namespace TravelCoreProject.Areas.Member.Controllers
 {
     [Area("Member")]
+
+   
     public class ReservationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        ReservationManager reservationManager = new ReservationManager(new EfReservationDal());
         public IActionResult MyCurrentResevation()
         {
             return View();
@@ -22,13 +29,25 @@ namespace TravelCoreProject.Areas.Member.Controllers
         [HttpGet]
         public IActionResult NewReservation()
         {
+            List<SelectListItem> values = (from x in destinationManager.TGetlist()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.City,
+                                               Value = x.DestinationId.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
             return View();
         }
 
         [HttpPost]
         public IActionResult NewReservation(Reservation p)
         {
-            return View();
+            p.AppUserId = 1;
+            p.Status = "Onay Bekliyor";
+            reservationManager.TAdd(p);
+
+            return RedirectToAction("MyCurrentResrvation");
+         
         }
 
         
