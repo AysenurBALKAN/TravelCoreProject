974b63b4e703c2ec9822104ac2e8cf02913c40f3
AysenurBALKAN <balkan.aysenur@gmail.com>
2024-03-08 08:30:12 +0300

    Guides Name and Profile Information Listed

---
+++ ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TBusinessLayer.Concrete;
+using TDataAccesLayer.EntityFramework;
+
+namespace TravelCoreProject.ViewComponents.MemberDashboard
+{
+    public class _GuideList:ViewComponent
+    {
+        GuideManager guideManager = new GuideManager(new EfGuideDal());
+
+
+        public IViewComponentResult Invoke()
+        {
+            var values = guideManager.TGetlist();
+            return View(values);
+        }
+    }
+    
+}
---
+++ ViewComponents/MemberDashboard/_ProfileInformation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TEntityLayer.Concrete;
+
+namespace TravelCoreProject.ViewComponents.MemberDashboard
+{
+    public class _ProfileInformation:ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public _ProfileInformation(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task< IViewComponentResult> InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.membername = values.Name + " " + values.Surname;
+            ViewBag.memmberPhone = values.PhoneNumber;
+            ViewBag.memberMail = values.Email;
+            return View();
+        }
+    }
+}
