cd1615950f4fabaf3aebb3959927b13957e8f9d0
AysenurBALKAN <balkan.aysenur@gmail.com>
2024-03-09 08:41:33 +0300

    Admin Area and Admin Dashboard Prepared

--- Startup.cs
+++ Startup.cs
@@ -77,6 +77,13 @@ namespace TravelCoreProject
                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                 );
             });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
