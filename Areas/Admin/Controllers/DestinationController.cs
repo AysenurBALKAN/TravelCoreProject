87ea047d36d7a1894632e8abcca8f6b051215a8e
AysenurBALKAN <balkan.aysenur@gmail.com>
2024-03-10 10:58:26 +0300

    Admin's Destination Page Prepared

--- Areas/Admin/Controllers/DestinationController.cs
+++ Areas/Admin/Controllers/DestinationController.cs
@@ -3,15 +3,55 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TBusinessLayer.Concrete;
+using TDataAccesLayer.EntityFramework;
+using TEntityLayer.Concrete;
 
 namespace TravelCoreProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class DestinationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
+        [HttpGet]
         public IActionResult Index()
         {
+            var values = destinationManager.TGetlist();
+            return View(values);
+        } 
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            destinationManager.TAdd(destination);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult AddDestination()
+        {
             return View();
+        }
+
+        public IActionResult DeleteDestination(int id)
+        {
+            var values = destinationManager.TGetById(id);
+            destinationManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateDestination(int id)
+        {
+            var values = destinationManager.TGetById(id);
+            return View(values);
+        }
+        
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            destinationManager.TUpdate(destination);
+            return RedirectToAction("Index");
         }
     }
 }
