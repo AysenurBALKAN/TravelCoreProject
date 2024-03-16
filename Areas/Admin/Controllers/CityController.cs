b391aadc66f5b82faba1c18e162a17f51e0586cd
AysenurBALKAN <balkan.aysenur@gmail.com>
2024-03-16 11:17:09 +0300

    ajax operations part1-part2

---
+++ Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TBusinessLayer.Abstract;
+using TEntityLayer.Concrete;
+using TravelCoreProject.Models;
+
+namespace TravelCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        private readonly IDestinationService _destinationService;
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(_destinationService.TGetlist());
+            return Json(jsonCity);
+        }
+
+        [HttpPost]
+        public IActionResult AddCityDestination(Destination p)
+        {
+            p.status = true;
+            _destinationService.TAdd(p);
+            var values = JsonConvert.SerializeObject(p);
+            return Json(values);
+        }
+
+ 
+        public IActionResult GetById(int DestinationId)
+        {
+          var values=  _destinationService.TGetById(DestinationId);
+            var jsonValues = JsonConvert.SerializeObject(values);
+            return Json(jsonValues);
+          
+        }
+        //public static List<CityClass> Cities = new List<CityClass>
+        //{
+        //    new CityClass
+        //    {
+        //        CityId=1,
+        //        CityName="Sarajeva",
+        //        CityCountry="Bosna-Hersek"
+        //    }
+
+        //};
+
+        
+    }
+}
