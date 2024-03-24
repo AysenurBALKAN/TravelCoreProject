bfe2ebf4a2db72ba25047c6ca745843daa0759df
Ayşenur Balkan <balkan.aysenur@gmail.com>
2024-03-24 10:41:04 +0300

    Add files via upload

---
+++ TDataAccesLayer/EntityFramework/EfAccountDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDataAccesLayer.Abstract;
+using TDataAccesLayer.Concrete;
+using TDataAccesLayer.Repository;
+using TEntityLayer.Concrete;
+
+namespace TDataAccesLayer.EntityFramework
+{
+   public class EfAccountDal:GenericUnitOfWork<Account>,IAccountDal
+    {
+        public EfAccountDal(Context context):base(context)
+        {
+           
+        }
+    }
+}
---
+++ TDataAccesLayer/EntityFramework/EfComment.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDataAccesLayer.Abstract;
+using TDataAccesLayer.Concrete;
+using TDataAccesLayer.Repository;
+using TEntityLayer.Concrete;
+
+namespace TDataAccesLayer.EntityFramework
+{
+    public class EfComment : GenericRepository<Comment>, ICommentDal
+    {
+        public List<Comment> GetListCommentWithDestination()
+        {
+            using (var c = new Context()) {
+                return c.Comments.Include(x => x.Destination).ToList();
+                    }
+        }
+    }
+}
